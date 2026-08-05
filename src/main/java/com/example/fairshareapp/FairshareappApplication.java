29ac7b47c0e9ba7e1cf9ebefad3c42a319c91860
Thomas Uriel Fernández <thomas.fernandez@coto.com.ar>
2026-08-05 20:24:55 -0300

    feat: initialize Spring Boot project with Gradle wrapper and basic configuration

---
+++ src/main/java/com/example/fairshareapp/FairshareappApplication.java
@@ -0,0 +1,13 @@
+package com.example.fairshareapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FairshareappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FairshareappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fairshareapp/FairshareappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fairshareapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FairshareappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
