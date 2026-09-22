f13cf11e1e48ecd2558a666cccfb8d8979c9b8e6
macarenageorgina <macarenageorginag@gmail.com>
2026-09-21 22:53:42 -0300

    feat: implementar QueryMethods para filtros de fecha y evitar omisiones

--- src/main/java/com/example/fairshareapp/service/GastoService.java
+++ src/main/java/com/example/fairshareapp/service/GastoService.java
@@ -155,6 +155,10 @@ public class GastoService {
                 throw new ReglaInvalidaException("La fecha desde no puede ser posterior a la fecha hasta");
             }
             gastos = gastoRepository.findByEspacioIdAndFechaBetweenOrderByFechaDesc(espacioId, fechaDesde, fechaHasta);
+        } else if (fechaDesde != null) {
+            gastos = gastoRepository.findByEspacioIdAndFechaGreaterThanEqualOrderByFechaDesc(espacioId, fechaDesde);
+        } else if (fechaHasta != null) {
+            gastos = gastoRepository.findByEspacioIdAndFechaLessThanEqualOrderByFechaDesc(espacioId, fechaHasta);
         } else {
             gastos = gastoRepository.findByEspacioIdOrderByFechaDesc(espacioId);
         }
